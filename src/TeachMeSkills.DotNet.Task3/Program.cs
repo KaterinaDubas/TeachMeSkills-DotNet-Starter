16cc71a3e9761f1bc33f14d8e1e1ee67bd7c7563
KaterinaDubas <70284894+KaterinaDubas@users.noreply.github.com>
2020-09-18 21:41:27 +0300

    feat: solution of 3 task

---
+++ src/TeachMeSkills.DotNet.Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double r = Convert.ToDouble(Console.ReadLine());
+            double s = pi * r * r;
+            Console.WriteLine(s);
+
+
+            
+        }
+    }
+}
