a3d7152f66ee139d2d78fa2484e736a0dc0e28d0
KaterinaDubas <70284894+KaterinaDubas@users.noreply.github.com>
2020-09-22 20:46:07 +0300

    feat: add solution of 5 task

---
+++ TeachMeSkills.DotNet.Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the value of variable a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the value of variable b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"a + b =  {a+b}");
+            Console.WriteLine($"a - b =  {a-b}");
+            Console.WriteLine($"a * b =  {a*b}");
+            Console.WriteLine($"a / b =  {a/b}");
+            Console.ReadKey();
+        }
+    }
+}
