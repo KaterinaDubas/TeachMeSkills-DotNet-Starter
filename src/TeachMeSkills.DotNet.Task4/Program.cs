b07f867136fd653d075119f2bde07cabcfc75701
KaterinaDubas <70284894+KaterinaDubas@users.noreply.github.com>
2020-09-22 14:05:42 +0300

    feat: add solution of 4 task

---
+++ src/TeachMeSkills.DotNet.Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal R = 7.14M;
+            decimal h = 3.84M;
+            decimal pi = 3.141592653M;
+            Console.WriteLine("V = " + (pi*R*R*h));
+            Console.WriteLine("S = " + (2*pi*R*R*h+2*pi*R*R));
+            Console.ReadKey();
+        }
+    }
+}
