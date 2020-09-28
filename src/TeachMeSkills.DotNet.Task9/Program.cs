8703d1e129279a7e3655422ad03ff29b00e43f34
KaterinaDubas <70284894+KaterinaDubas@users.noreply.github.com>
2020-09-28 23:08:16 +0300

    feat: add solution 9 task

---
+++ src/TeachMeSkills.DotNet.Task9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string a = Console.ReadLine();
+            int number = int.Parse(a);
+            int result;
+            int resedue = Math.DivRem(number, 2, out result);
+            if (result == 0)
+            {
+                Console.WriteLine("even number");
+            }
+            else
+            {
+                Console.WriteLine("odd number");
+            }
+
+            Console.Write("Enter a number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (b % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+        }
+    }
+}
