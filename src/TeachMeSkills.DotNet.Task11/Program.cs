78b80fe1d591504c60d80e54ec08ac97944af3d6
KaterinaDubas <70284894+KaterinaDubas@users.noreply.github.com>
2020-09-29 10:35:36 +0300

    feat: add solution of 11 task

---
+++ src/TeachMeSkills.DotNet.Task11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool degree = result(number);
+            if (degree == true)
+            {
+                Console.WriteLine("degree of 2");
+            }
+            else
+            {
+                Console.WriteLine("not degree of 2");
+            }
+        }
+
+        static bool result(int number)
+        {
+            if (number == 2) return true;
+            else if (number % 2 == 0) return result(number / 2);
+            else return false;
+        }
+    }
+}
