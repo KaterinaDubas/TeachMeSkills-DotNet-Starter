f436133b39dcb467066c5bda4b75c25906230c8d
KaterinaDubas <70284894+KaterinaDubas@users.noreply.github.com>
2020-10-19 00:14:25 +0300

    feat: add solution of 13 task

---
+++ src/TeachMwSkills.DotNet.Task12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TeachMwSkills.DotNet.Task13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter number B (shoud be bigger than A): ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            Console.Write("Odd numbers: ");
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.Write($"{i},");
+                }
+                sum += i;
+            }
+            Console.WriteLine($"\nSum of numbers between A and B = {sum}");
+            Console.ReadKey();
+        }
+    }
+}
