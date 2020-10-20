f7dab774edde61c51043b179255a604fe8ef00b4
KaterinaDubas <70284894+KaterinaDubas@users.noreply.github.com>
2020-10-20 23:05:15 +0300

    feat: add solution of 16 task

---
+++ src/TeachMeSkills.DotNet.Task16/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            int y = 7;
+            for (int i = 0; i < y; i++)
+            {
+                for (int t = 0; t < x; t++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+
+            int lenght = 20;
+            int height = 7;
+            int q = 0;
+            int w = 10;
+            Console.SetCursorPosition(q, w);
+            for (int t = 0; t < height; t++)
+            {
+                for (int i = 0; i < lenght; i++)
+                {
+                    if (t == 0 || t == height - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else if (i == 0 || i == lenght - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
