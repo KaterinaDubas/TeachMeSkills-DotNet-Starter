66850dac895f5dda0d175ba580cf55d6bf15f058
KaterinaDubas <70284894+KaterinaDubas@users.noreply.github.com>
2020-11-05 23:27:22 +0300

    feat: add solution of 18 task

---
+++ src/TeachMeSkills.DotNet.Task18/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter some number: ");
+            int x = int.Parse(Console.ReadLine());
+            NumbersCheker(x);
+            Console.ReadKey();
+        }
+
+        public static void NumbersCheker(int x)
+        {
+            bool prime = true;
+            if (x >= 0)
+            {
+                Console.WriteLine("Number is positive");
+            }
+            else
+            {
+                Console.WriteLine("Number is negative");
+            }
+
+            for(int i = 2; i <= x / 2; i++)
+            {
+                if (x % i == 0)
+                {
+                    prime = false;
+                    Console.WriteLine("The number is not prime");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("The number is prime");
+                    break;
+                }
+            }
+
+            if (x % 2 == 0 && x % 3 == 0 && x % 5 == 0 && x % 6 == 0 && x % 9 == 0)
+            {
+                Console.WriteLine("The number is divided without remainder by 2, 3, 5, 6 and 9.");
+            }
+            else
+            {
+                Console.WriteLine("The number isn't divisible by 2, 3, 5, 6, 9 without remainder.");
+            }
+        }
+    }
+}
